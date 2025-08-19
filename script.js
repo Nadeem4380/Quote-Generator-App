e138adec82b2684fb053f7042ec9c05b8a1c02d6
Nadeem Ansari <94752890+Nadeem4380@users.noreply.github.com>
2025-08-19 13:23:23 +0000

    made changes to script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const quotes = [
+    { text: "The only limit t our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt" },
+    { text: "In the middle of every difficulty lies opportunity.", author: "Albert Einstein" },
+    { text: "What you get by achieving your goals is not as important as what you become by achieving your goals.", author: "Zig Ziglar" },
+    { text: "Dream big and dare to fail.", author: "Norman Vaughan" },
+    { text: "You are never too old to set another goal or to dream a new dream.", author: "C.S. Lewis"}
+];
 
+const quoteText = document.getElementById('quote-text');
+const quoteAuthor = document.getElementById('quote-author');
+const newQuotebtn = document.getElementById('new-quote');
+
+function getRandomQuote(){
+    let index = Math.floor(Math.random() * quotes.length);
+    return quotes[index];
+}
+
+function displayQuote(){
+    let quote = getRandomQuote();
+    quoteText.textContent = `"${quote.text}"`;
+    quoteAuthor.textContent = `- ${quote.author}`;
+}
+
+newQuotebtn.addEventListener('Click', displayQuote);
+
+window.onload = displayQuote;
